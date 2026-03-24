0d2bf633e9112a4e96c12a2a4e4d8b0a01618b54
Khayal Sharifov <sharifovkhayal6@gmail.com>
2026-03-24 14:31:03 +0400

    feat: add persistence and custom exception handling for file module
    - Create FileRecordRepository for database operations
    - Implement FileOperationException for handling runtime file errors

---
+++ src/main/java/com/bmu1093a/quill/file/exception/FileOperationException.java
@@ -0,0 +1,15 @@
+package com.bmu1093a.quill.file.exception;
+
+import lombok.Getter;
+
+@Getter
+public class FileOperationException extends RuntimeException {
+
+    private final String code;
+
+    public FileOperationException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+}
---
+++ src/main/java/com/bmu1093a/quill/file/repo/FileRecordRepository.java
@@ -0,0 +1,10 @@
+package com.bmu1093a.quill.file.repo;
+
+import com.bmu1093a.quill.file.model.entity.FileRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FileRecordRepository extends JpaRepository<FileRecord, Long> {
+    Optional<FileRecord> findByPublicIdAndDeletedFalse(String publicId);
+}
